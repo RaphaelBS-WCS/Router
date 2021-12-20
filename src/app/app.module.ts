f565cbf7fc173d6ddac3b9e764e5538062a672b8
Niurwa <raphaelbilletservoin@gmail.com>
2021-12-20 16:34:39 +0100

    Quest Done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {RouterModule} from "@angular/router";
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { SignUpComponent } from './menu/sign-up/sign-up.component';
+import { UserProfileComponent } from './menu/user-profile/user-profile.component';
+import { ROUTES } from "./app.routes";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    SignUpComponent,
+    UserProfileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
